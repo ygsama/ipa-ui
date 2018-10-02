5c360613481abe66a8e92ddcb9e7bdd2b79124e6
ygsama <ygsama@qq.com>
2018-10-02 17:17:30 +0800

    布局

--- src/app/blog/home/home.component.ts
+++ src/app/blog/home/home.component.ts
@@ -12,6 +12,14 @@ export class HomeComponent implements OnInit {
   key: string = '';
   urlstr: string = '';
 
+  data = [
+    {'title':'Racing car sprays burning fuel into crowd.','time':'2018-01-01 18:36:20','id':'123'},
+    {'title':'Japanese princess to wed commoner.','time':'2018-01-01 18:36:20','id':'1234'},
+    {'title':'Australian walks 100km after outback crash.','time':'2018-01-01 18:36:20','id':'1243'},
+    {'title':'Man charged over missing wedding girl.','time':'2018-01-01 18:36:20','id':'1423'},
+    {'title':'Los Angeles battles huge wildfires.','time':'2018-01-01 18:36:20','id':'1235'},
+    {'title':'Racing car sprays burning fuel into crowd.','time':'2018-01-01 18:36:20','id':'4123'}
+  ];
   constructor() {
   }
 
@@ -29,6 +37,19 @@ export class HomeComponent implements OnInit {
     }
     this.urlstr += this.key;
     localStorage.setItem('urls', this.urlstr);
+  }
+
+
+  index = 0;
+  tabs = [ 'Tab 1', 'Tab 2' ];
+
+  closeTab(tab: string): void {
+    this.tabs.splice(this.tabs.indexOf(tab), 1);
+  }
+
+  newTab(): void {
+    this.tabs.push('New Tab');
+    this.index = this.tabs.length - 1;
   }
 
 
